ade49cf6479e8bfec62cfdfc0597a2d499af5bc9
Vane <vanesahabbo07@gmail.com>
2022-05-06 00:18:20 -0300

    Agregado de index

--- ProyectoFinal/urls.py
+++ ProyectoFinal/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
+from MiApp.views import inicio
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',inicio),
+    path('MiApp/',include('MiApp.urls'))
 ]
